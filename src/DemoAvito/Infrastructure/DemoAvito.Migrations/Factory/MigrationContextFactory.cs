9c408197279d93cb271e237eee74f5dbb9ee4826
enverzeynaliev <fizrukcom62@gmail.com>
2022-11-17 23:49:01 +0300

    Поправил файл настроек приложения.  Настроил и создал миграцию -  Initial_Create

--- src/DemoAvito/Infrastructure/DemoAvito.Migrations/Factory/MigrationContextFactory.cs
+++ src/DemoAvito/Infrastructure/DemoAvito.Migrations/Factory/MigrationContextFactory.cs
@@ -3,30 +3,29 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
-namespace DemoAvito.Migrations.Factory
+namespace DemoAvito.Migrations.Factory;
+
+public class MigrationContextFactory : IDesignTimeDbContextFactory<MigrationsDbContext>
 {
-    public class MigrationContextFactory : IDesignTimeDbContextFactory<MigrationsDbContext>
+    public MigrationsDbContext CreateDbContext(string[] args)
     {
-        public MigrationsDbContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<MigrationsDbContext>();
+        var optionsBuilder = new DbContextOptionsBuilder<MigrationsDbContext>();
 
-            // получаем конфигурацию из файла appsettings.json
-            Microsoft.Extensions.Configuration.ConfigurationBuilder builder =
-                new Microsoft.Extensions.Configuration.ConfigurationBuilder();
-            builder.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
-            builder.AddJsonFile("appsettings.json");
-            IConfigurationRoot config = builder.Build();
+        // получаем конфигурацию из файла appsettings.json
+        Microsoft.Extensions.Configuration.ConfigurationBuilder builder =
+            new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        builder.AddJsonFile("appsetting.json");
+        IConfigurationRoot config = builder.Build();
 
-            // получаем строку подключения из файла appsettings.json
-            string connectionString = config.GetConnectionString("PostgresAdvertDb");
-            optionsBuilder.UseNpgsql(connectionString, opts => opts
-                .CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds)
-                .MigrationsAssembly(typeof(MigrationContextFactory).Assembly.FullName)
-            );
+        // получаем строку подключения из файла appsettings.json
+        string? connectionString = config.GetConnectionString("PostgresAdvertDb");
+        optionsBuilder.UseNpgsql(connectionString, opts => opts
+            .CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds)
+            .MigrationsAssembly(typeof(MigrationContextFactory).Assembly.FullName)
+        );
 
 
-            return new MigrationsDbContext(optionsBuilder.Options);
-        }
+        return new MigrationsDbContext(optionsBuilder.Options);
     }
 }
---
+++ src/DemoAvito/Infrastructure/DemoAvito.Migrations/Migrations/20221117203052_Initial_Create.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoAvito.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CategoryName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CitiesTable",
+                columns: table => new
+                {
+                    CityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    NameCity = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CitiesTable", x => x.CityId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DemoAvitoDtoAdverts",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "character varying(800)", maxLength: 800, nullable: true),
+                    CreationDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    SubCategory = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    ProductPrice = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DemoAvitoDtoAdverts", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_DemoAvitoDtoAdverts_Categories_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DemoAvitoDtoAdverts_CitiesTable_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "CitiesTable",
+                        principalColumn: "CityId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    PictureId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Photo = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.PictureId);
+                    table.ForeignKey(
+                        name: "FK_Images_DemoAvitoDtoAdverts_PictureId",
+                        column: x => x.PictureId,
+                        principalTable: "DemoAvitoDtoAdverts",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "DemoAvitoDtoAdverts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "CitiesTable");
+        }
+    }
+}
