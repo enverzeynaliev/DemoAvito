abc894c5c7da114dc0a8c49bea65e27b99888d18
enverzeynaliev <fizrukcom62@gmail.com>
2022-11-17 17:57:50 +0300

    Переопределил конфигураторы моделей

--- src/DemoAvito/Infrastructure/DemoAvito.DataAccess/AdvertContext.cs
+++ src/DemoAvito/Infrastructure/DemoAvito.DataAccess/AdvertContext.cs
@@ -1,15 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DemoAvito.DataAccess.EntityConfiguration.Category;
+using DemoAvito.DataAccess.EntityConfiguration.Cities;
+using DemoAvito.DataAccess.EntityConfiguration.DemoAvito;
+using DemoAvito.DataAccess.EntityConfiguration.Pictures;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoAvito.DataAccess;
 
 public class AdvertContext : DbContext
 {
-   /// <summary>
-   /// Инициализирует экземпляр <see cref="AdvertContext"/> 
-   /// </summary>
-   /// <param name="options"></param>
+    /// <summary>
+    /// Инициализирует экземпляр <see cref="AdvertContext"/> 
+    /// </summary>
+    /// <param name="options"></param>
     public AdvertContext(DbContextOptions options)
         : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AdvertConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new CityConfiguration());
+        modelBuilder.ApplyConfiguration(new PhotosConfiguration());
     }
 }
